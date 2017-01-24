07338dadf19823b41936d2d5b42a16f53698fc47
jaclynb <jaclynb@mtu.edu>
2017-01-24 13:22:16 -0500

    Complete dances

--- robotdf.py
+++ robotdf.py
@@ -2,8 +2,8 @@
 
 """
 Usage:
-  berceuse.py ROBOT_IP SONG
-  berceuse.py (-h | --help)
+  robotdf.py ROBOT_IP SONG
+  robotdf.py (-h | --help)
 Options:
   -h, --help
 """
@@ -15,75 +15,86 @@ import logging.config
 import datetime
 import time
 
-songparts =  {'Berceuse': 5,
-              'Tisket': 5,
-              'Twinkle': 5,
-              'Waltz': 4}
+import berceuseDance
+import tisketDance
+import twinkleDance
+import waltzDance
 
-songlength = {'Berceuse': [11, 9, 14, 7, 7],
-              'Tisket': [18, 14, 14, 12, 13],
-              'Twinkle': [7, 5, 10, 5, 10], 
-              'Waltz': [10, 8, 13, 12]}
+berceuse = berceuseDance.berceuse
+tisket = tisketDance.tisket
+twinkle = twinkleDance.twinkle
+waltz = waltzDance.waltz
 
-pauses =    {'Berceuse': [3, 2, 4, 2],
-             'Tisket': [3, 3, 2],
-             'Twinkle': [2, 1, 3, 2],
-             'Waltz': [3, 2, 4]}
 
 def setuplogging():
-    logger = logging.getLogger("humandf")
+
+    logger = logging.getLogger("robotdf")
     logger.setLevel(logging.INFO)
- 
+
     # create the logging file handler
     now = datetime.datetime.now()
     log_filename = "log\\robotdf_" + now.strftime('%Y_%m_%d_%H_%M_%S') + ".log"
     fh = logging.FileHandler(log_filename)
- 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
- 
+
     # add handler to logger object
     logger.addHandler(fh)
 
     return (logger)
 
+
 def main(arguments):
-  """
-  Play and log music for human Dance Freeze
-  """
+    """
+    Play and log music for human Dance Freeze
+    """
 
-  logger = setuplogging()
+    logger = setuplogging()
 
-  robotIP = arguments.pop("ROBOT_IP", None)
-  if (robotIP == None):
-    robotIP = "127.0.0.1"
+    robotIP = arguments.pop("ROBOT_IP", None)
+    if (robotIP is None):
+        robotIP = "127.0.0.1"
 
-  port = 9559
+    port = 9559
 
-  song = arguments.pop("SONG", None)
-  if (song == None):
-    sys.exit("Need song argument")
+    song = arguments.pop("SONG", None)
+    if (song is None):
+        sys.exit("Need song argument")
 
-  audio = ALProxy("ALAudioPlayer", robotIP, port)
-  motion = ALProxy("ALMotion", robotIP, port)
-  posture = ALProxy("ALRobotPosture", robotIP, port)
+    if (song == "Berceuse"):
+        songObj = berceuse
+    elif (song == "Tisket"):
+        songObj = tisket
+    elif (song == "Twinkle"):
+        songObj = twinkle
+    elif (song == "Waltz"):
+        songObj = waltz
+    else:
+        sys.exit("Invalid song name")
 
-  motion.wakeUp()
-  posture.goToPosture("StandInit", 0.5)
+    audio = ALProxy("ALAudioPlayer", robotIP, port)
+    motion = ALProxy("ALMotion", robotIP, port)
+    posture = ALProxy("ALRobotPosture", robotIP, port)
 
-  for i in range(songparts[song]):
-    songfile = "/var/persistent/home/nao/dancefreeze/music/" + song + str(i+1) + ".wav"
-    logger.info("Start " + song + str(i+1))
-    audio.playFile(songfile)
-    logger.info("End " + song + str(i+1))
-    if (i < len(pauses[song])):
-      time.sleep(pauses[song][i])
-      logger.info("Pause of " + str(pauses[song][i]) + " secs")
+    motion.wakeUp()
+    posture.goToPosture("Stand", 0.8)
 
-  posture.goToPosture("StandInit", 0.5)
-  motion.rest()
+    for i in range(songObj.get_num_parts()):
+        songfile = "/var/persistent/home/nao/dancefreeze/music/" + \
+            songObj.get_song_name() + str(i + 1) + ".wav"
+        logger.info("Start " + songObj.get_song_name() + str(i + 1))
+        audio.post.playFile(songfile)
+        motion.angleInterpolation(songObj.get_part(i).get_names(),
+                                  songObj.get_part(i).get_keys(),
+                                  songObj.get_part(i).get_times(), True)
+        logger.info("End " + songObj.get_song_name() + str(i + 1))
+        time.sleep(songObj.get_part(i).get_pause_length())
+
+    posture.goToPosture("Stand", 0.8)
 
 if __name__ == "__main__":
-  arguments = docopt(__doc__)
+    arguments = docopt(__doc__)
 
-  main(arguments)
+    main(arguments)
